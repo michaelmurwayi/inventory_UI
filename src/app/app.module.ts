49eda5386ade1188a2ac835d03f81d0f90546863
Alex Zamora <karlazamora@berkeley.edu>
2022-03-23 16:27:53 +0300

    adding home view and home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home-view/home-main/home.component';
+import { HomeNavComponent } from './components/home-view/home-nav/home-nav.component';
+import { HomeFooterComponent } from './components/home-view/home-view/home-footer/home-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeNavComponent,
+    HomeFooterComponent
   ],
   imports: [
     BrowserModule,
