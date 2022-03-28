42eb2c739b411c530a4d92d50dfa8772bc0d03e7
Alex Zamora <karlazamora@berkeley.edu>
2022-03-28 16:09:20 +0300

    moving home view components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,22 @@ import { environment } from 'src/environments/environment';
 import { AppState } from './state/app.state';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home-view/home-main/home.component';
+import { HomeMainComponent } from './components/home-view/home-main/home-main.component';
 import { HomeNavComponent } from './components/home-view/home-nav/home-nav.component';
+import { StockListComponent } from './stock-view/stock-list/stock-list.component';
+import { CartComponent } from './stock-view/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+
 // import { HomeFooterComponent } from './components/home-view/home-view/home-footer/home-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HomeNavComponent
+    HomeMainComponent,
+    HomeNavComponent,
+    StockListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
