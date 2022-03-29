0b046e7a7c2f12a8e937efe6d0fcb407886c3c84
michael murwayi <mikemurwayi7@gmail.com>
2022-03-29 23:24:29 +0300

    adding product module

---
+++ src/app/models/Product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id: string,
+    product_name: string;
+    product_price: number;
+    quantity: number;
+    lastUpdated: string;
+    dateCreated: string;
+    isDeleted: boolean;
+}
