edad3c361b4248b0c831b47145be2ae72fd749ce
michael murwayi <mikemurwayi7@gmail.com>
2022-04-12 05:30:28 +0300

    adding map div

--- src/app/components/admin/bargraph/bargraph.component.ts
+++ src/app/components/admin/bargraph/bargraph.component.ts
@@ -12,10 +12,11 @@ export class BargraphComponent implements OnInit {
   chartType = 'bar';
 
   chartDatasets = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' }
+    { data: [65, 59, 80, 81, 56, 55, 40, 65, 80, 81, 56, 55], label: 'My First dataset' },
+    { data: [65, 59, 80, 81, 56, 55, 40, 65, 80, 81, 56, 55], label: 'My First dataset' }
   ];
 
-  chartLabels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+  chartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
   chartColors = [
     {
@@ -25,7 +26,12 @@ export class BargraphComponent implements OnInit {
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
         'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(0 , 0 ,0 , 0.2)', ,
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
       ],
       borderColor: [
         'rgba(255,99,132,1)',
@@ -33,7 +39,12 @@ export class BargraphComponent implements OnInit {
         'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
         'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
+        'rgba(255, 159, 64, 1)',
+        'rgba(0 , 0 ,0 , 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',  
       ],
       borderWidth: 2,
     }
