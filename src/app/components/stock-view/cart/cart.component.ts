5d6c5d3de03fd376751085fdeabe79f417992dd9
michael murwayi <mikemurwayi7@gmail.com>
2022-06-18 11:19:56 +0300

    Feature<dashboard>: Cart. Get total and subtotal of items in cart

--- src/app/components/stock-view/cart/cart.component.ts
+++ src/app/components/stock-view/cart/cart.component.ts
@@ -11,7 +11,10 @@ export class CartComponent implements OnInit {
 
   myDate?: any;
   time?: any;
-  cart : any;
+  cart: any;
+  sub_total?: number;
+  total?: number;
+  tax = 0.00;
 
   constructor(private CartService: CartService) { }
 
@@ -19,11 +22,21 @@ export class CartComponent implements OnInit {
     this.cart = this.CartService.getCart();
     return this.cart;
   }
+  getTotalPrice() {
+    let total = 0;
+    this.cart.forEach((item: { price: number; quantity: number; }) => {
+      total += item.price * item.quantity;
+    }
+    );
+    return total;
+  }
 
   ngOnInit(): void {
     this.myDate = new Date();
     this.cart = this.getCart();
-    console.log("cart is ", this.cart)
+    this.sub_total = this.getTotalPrice();
+    this.total = this.sub_total + this.tax;
+
 
   }
 
